9d991467183f49a50fa426021c6e42e4bfade1d8
Charan-TM <charan.maanam@techmojo.in>
2026-02-19 00:52:34 +0530

    Revert: remove shadow node, simple ViewManager only

--- android/src/main/java/com/reactnativemintsdk/MintHelloViewManager.kt
+++ android/src/main/java/com/reactnativemintsdk/MintHelloViewManager.kt
@@ -1,6 +1,5 @@
 package com.reactnativemintsdk
 
-import com.facebook.react.uimanager.LayoutShadowNode
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.mint.sdk.MintHelloView
@@ -11,13 +10,5 @@ class MintHelloViewManager : SimpleViewManager<MintHelloView>() {
 
     override fun createViewInstance(reactContext: ThemedReactContext): MintHelloView {
         return MintHelloView(reactContext)
-    }
-
-    override fun getShadowNodeClass(): Class<out LayoutShadowNode> {
-        return MintHelloShadowNode::class.java
-    }
-
-    override fun createShadowNodeInstance(): LayoutShadowNode {
-        return MintHelloShadowNode()
     }
 }
